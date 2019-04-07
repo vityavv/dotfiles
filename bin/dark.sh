e1838079e3d6fd43271cda1997bb249d6ab8dc96
Victor Veytsman <vityavv@gmail.com>
2019-04-07 13:11:07 -0400

    Switch to papercolor

--- bin/dark.sh
+++ bin/dark.sh
@@ -1,30 +1,28 @@
-convert -size 1x1 canvas:\#282828 ~/background_pixel.png
-feh --bg-scale ~/background_pixel.png
-sed -ri 's/^color-normal.+/color-normal: "#282828, #ebdbb2, #282828, #ebdbb2, #282828";/g' ~/.config/rofi/config.rasi
-sed -ri 's/color-window.+/color-window: "#282828, #b8bb26, #b8bb26";/g' ~/.config/rofi/config.rasi
-sed -i 's/gruvbox_light/gruvbox_dark/' ~/.config/openbox/rc.xml
+sed -ri 's/^color-normal.+/color-normal: "#2e2e2d, #f2f2f2, #2e2e2d, #f2f2f2, #2e2e2d";/g' ~/.config/rofi/config.rasi
+sed -ri 's/color-window.+/color-window: "#2e2e2d, #90c94d, #90c94d";/g' ~/.config/rofi/config.rasi
+sed -i 's/papercolor_light/papercolor_dark/' ~/.config/openbox/rc.xml
 sed -i 's/light/dark/' ~/.vimrc
 openbox --reconfigure
 ~/bin/dark.py
 for file in $(ls /dev/pts)
 do
 	[ $file == "ptmx" ] && continue
-	echo -ne '\033]11;#282828\007' > "/dev/pts/$file"
-	echo -ne '\033]10;#ebdbb2\007' > "/dev/pts/$file"
-	echo -ne '\033]4;0;#282828\007' > "/dev/pts/$file"
-	echo -ne '\033]4;8;#928374\007' > "/dev/pts/$file"
-	echo -ne '\033]4;1;#cc241d\007' > "/dev/pts/$file"
-	echo -ne '\033]4;9;#fb4934\007' > "/dev/pts/$file"
-	echo -ne '\033]4;2;#98971a\007' > "/dev/pts/$file"
-	echo -ne '\033]4;10;#b8bb26\007' > "/dev/pts/$file"
-	echo -ne '\033]4;3;#d79921\007' > "/dev/pts/$file"
-	echo -ne '\033]4;11;#fabd2f\007' > "/dev/pts/$file"
-	echo -ne '\033]4;4;#458588\007' > "/dev/pts/$file"
-	echo -ne '\033]4;12;#83a598\007' > "/dev/pts/$file"
-	echo -ne '\033]4;5;#b16286\007' > "/dev/pts/$file"
-	echo -ne '\033]4;13;#d3869b\007' > "/dev/pts/$file"
-	echo -ne '\033]4;6;#689d6a\007' > "/dev/pts/$file"
-	echo -ne '\033]4;14;#8ec07c\007' > "/dev/pts/$file"
-	echo -ne '\033]4;7;#a89984\007' > "/dev/pts/$file"
-	echo -ne '\033]4;15;#ebdbb2\007' > "/dev/pts/$file"
+	echo -ne '\033]11;#2e2e2d\007' > "/dev/pts/$file"
+	echo -ne '\033]10;#f2f2f2\007' > "/dev/pts/$file"
+	echo -ne '\033]4;0;#2c2c2c\008' > "/dev/pts/$file"
+	echo -ne '\033]4;1;#c62828\008' > "/dev/pts/$file"
+	echo -ne '\033]4;2;#558b2e\008' > "/dev/pts/$file"
+	echo -ne '\033]4;3;#f9a825\008' > "/dev/pts/$file"
+	echo -ne '\033]4;4;#1565c1\008' > "/dev/pts/$file"
+	echo -ne '\033]4;5;#6a1e9a\008' > "/dev/pts/$file"
+	echo -ne '\033]4;6;#00838f\008' > "/dev/pts/$file"
+	echo -ne '\033]4;7;#ffffff\008' > "/dev/pts/$file"
+	echo -ne '\033]4;8;#969694\008' > "/dev/pts/$file"
+	echo -ne '\033]4;9;#f15250\008' > "/dev/pts/$file"
+	echo -ne '\033]4;10;#86bd47\008' > "/dev/pts/$file"
+	echo -ne '\033]4;11;#f8e63a\008' > "/dev/pts/$file"
+	echo -ne '\033]4;12;#77b2f6\008' > "/dev/pts/$file"
+	echo -ne '\033]4;13;#b963c8\008' > "/dev/pts/$file"
+	echo -ne '\033]4;14;#25c3dc\008' > "/dev/pts/$file"
+	echo -ne '\033]4;15;#ffffff\008' > "/dev/pts/$file"
 done
--- bin/light.sh
+++ bin/light.sh
@@ -1,30 +1,28 @@
-convert -size 1x1 canvas:\#fbf1c7 ~/background_pixel.png
-feh --bg-scale ~/background_pixel.png
-sed -ri 's/^color-normal.+/color-normal: "#fbf1c7, #3c3836, #fbf1c7, #3c3836, #fbf1c7";/g' ~/.config/rofi/config.rasi
-sed -ri 's/color-window.+/color-window: "#fbf1c7, #b8bb26, #b8bb26";/g' ~/.config/rofi/config.rasi
-sed -i 's/gruvbox_dark/gruvbox_light/' ~/.config/openbox/rc.xml
+sed -ri 's/^color-normal.+/color-normal: "#eeeeee, #3c3c3b, #eeeeee, #3c3c3b, #eeeeee";/g' ~/.config/rofi/config.rasi
+sed -ri 's/^color-window.+/color-window: "#eeeeee, #90c94d, #90c94d";/g' ~/.config/rofi/config.rasi
 sed -i 's/dark/light/' ~/.vimrc
+sed -i 's/papercolor_dark/papercolor_light/' ~/.config/openbox/rc.xml
 openbox --reconfigure
 ~/bin/light.py
 for file in $(ls /dev/pts)
 do
 	[ $file == "ptmx" ] && continue
-	echo -ne '\033]11;#fbf1c7\007' > "/dev/pts/$file"
-	echo -ne '\033]10;#3c3836\007' > "/dev/pts/$file"
-	echo -ne '\033]4;0;#fbf1c7\007' > "/dev/pts/$file"
-	echo -ne '\033]4;8;#928374\007' > "/dev/pts/$file"
-	echo -ne '\033]4;1;#cc241d\007' > "/dev/pts/$file"
-	echo -ne '\033]4;9;#9d0006\007' > "/dev/pts/$file"
-	echo -ne '\033]4;2;#98971a\007' > "/dev/pts/$file"
-	echo -ne '\033]4;10;#79740e\007' > "/dev/pts/$file"
-	echo -ne '\033]4;3;#d79921\007' > "/dev/pts/$file"
-	echo -ne '\033]4;11;#b57614\007' > "/dev/pts/$file"
-	echo -ne '\033]4;4;#458588\007' > "/dev/pts/$file"
-	echo -ne '\033]4;12;#076678\007' > "/dev/pts/$file"
-	echo -ne '\033]4;5;#b16286\007' > "/dev/pts/$file"
-	echo -ne '\033]4;13;#8f3f71\007' > "/dev/pts/$file"
-	echo -ne '\033]4;6;#689d6a\007' > "/dev/pts/$file"
-	echo -ne '\033]4;14;#427b58\007' > "/dev/pts/$file"
-	echo -ne '\033]4;7;#7c6f64\007' > "/dev/pts/$file"
-	echo -ne '\033]4;15;#3c3836\007' > "/dev/pts/$file"
+	echo -ne '\033]11;#eeeeee\007' > "/dev/pts/$file"
+	echo -ne '\033]10;#3c3c3b\007' > "/dev/pts/$file"
+	echo -ne '\033]4;0;#f8f8f8\007' > "/dev/pts/$file"
+	echo -ne '\033]4;8;#2c2c2c\007' > "/dev/pts/$file"
+	echo -ne '\033]4;1;#d7005f\007' > "/dev/pts/$file"
+	echo -ne '\033]4;9;#ef5251\007' > "/dev/pts/$file"
+	echo -ne '\033]4;2;#718c00\007' > "/dev/pts/$file"
+	echo -ne '\033]4;10;#90c94d\007' > "/dev/pts/$file"
+	echo -ne '\033]4;3;#ff8f01\007' > "/dev/pts/$file"
+	echo -ne '\033]4;11;#ffa000\007' > "/dev/pts/$file"
+	echo -ne '\033]4;4;#006f87\007' > "/dev/pts/$file"
+	echo -ne '\033]4;12;#4271ae\007' > "/dev/pts/$file"
+	echo -ne '\033]4;5;#6a1e9a\007' > "/dev/pts/$file"
+	echo -ne '\033]4;13;#c774c1\007' > "/dev/pts/$file"
+	echo -ne '\033]4;6;#3c999f\007' > "/dev/pts/$file"
+	echo -ne '\033]4;14;#3e999f\007' > "/dev/pts/$file"
+	echo -ne '\033]4;7;#2c2c2c\007' > "/dev/pts/$file"
+	echo -ne '\033]4;15;#f5f5f5\007' > "/dev/pts/$file"
 done
